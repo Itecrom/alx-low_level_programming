216ca108b7b85a1cbf4a46f0a83f4720d5fdd792
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-08 11:24:55 +0200

    Print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Starts Here
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
