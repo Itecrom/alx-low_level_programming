d0ca5b3e30a9b6ae93041a8c4cd2087e2503d79f
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-08 11:23:05 +0200

    Print Number6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Starts Here
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
