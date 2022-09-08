e164d2a11821d7cd91b5c8c9e5404bbcbaabb8ed
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-08 09:33:48 +0200

    Alx low level programming task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * Name: Leonard JJ Mhone
+ * Motto: Wiser today than yesterday
+ */
+
+#include <unistd.h>
 
 /**
- * main - Starts here
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
  * Return: 1
  */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
