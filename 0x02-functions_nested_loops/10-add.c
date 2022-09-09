baeed9752f102c03d9d9b9a216f7ba03cd8fcf7d
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-09 18:11:35 +0200

    Adds two Values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two values
+  * @n1: 1st# in sequence
+  * @n2: 2nd# in sequence
+  *
+  * Return: Integer value
+  */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
