86efd8f2a7b60b5f0cff27ef9b37d2efc3bf9530
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-09 18:32:05 +0200

    Adds multiple of Numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,48 @@
+
+/*
+ * File: 100-times_table.c
+ * Author: Leonard JJ Mhone
+ */
+
 #include "main.h"
 
 /**
-  * print_times_table - Prints a multiplication table
-  * @n: Number is here
-  *
-  * Return: Matrices
-  */
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: Times table value to output.
+ */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int num, mult, prod;
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
+		for (num = 0; num <= n; num++)
 		{
-			for (y = 0; y <= n; y++)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				z = x * y;
-				if (z > 99)
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (z > 9)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar((prod / 10) + '0');
 				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
