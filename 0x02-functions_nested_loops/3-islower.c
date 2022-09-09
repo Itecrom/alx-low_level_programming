16474311d59c8335f5b045542b647c4ff2c7d4e2
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-09 16:41:19 +0200

    lower alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  * _islower - Checks for small letters
+  * @c: Check this char
+  *
+  * Return: 1 lowercase or 0 for anyother
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
