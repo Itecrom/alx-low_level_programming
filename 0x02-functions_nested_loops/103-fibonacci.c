fa997070470b777118d54f68780455aa4fe0d78e
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-09 19:35:34 +0200

    Fibonacci 2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - outputs sum of Even Fibonacci#
+  * Not more than 4000000.
+  *
+  * Return: Null
+  */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+
+		if (k % 2 == 0)
+			sum += k;
+
+		j = k - j;
+
+		++i;
+	}
+
+	printf("%ld\n", sum);
+	return (0);
+}
