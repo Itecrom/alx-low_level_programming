d5d74aad5a53bbda694ab1a3056a786aa8d1bca9
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-09 18:50:49 +0200

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -22,19 +22,19 @@ int main(void)
 		fib2 = sum;
 	}
 
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
+	fib1_half1 = fib1 / 4000000;
+	fib2_half1 = fib2 / 4000000;
+	fib1_half2 = fib1 % 4000000;
+	fib2_half2 = fib2 % 4000000;
 
 	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999)
 		{
 			half1 += 1;
-			half2 %= 10000000000;
+			half2 %= 4000000;
 		}
 
 		printf("%lu%lu", half1, half2);
