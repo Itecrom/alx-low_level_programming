c0954817222e0611922513dce6362cfda3a9206c
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-09 18:47:22 +0200

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the sum the two predeeing numbers
+  * main - Prints fibonacci from 1
   * less than 4000000.
   *
   * Return: Null
   */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
 
-	while (k + j < 4000000)
+	for (count = 0; count < 92; count++)
 	{
-		k += j;
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
 
-		if (k % 2 == 0)
-			sum += k;
-
-		j = k - j;
-
-		++i;
+		fib1 = fib2;
+		fib2 = sum;
 	}
 
-	printf("%ld\n", sum);
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+		if (fib1_half2 + fib2_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
+	}
+	printf("\n");
 	return (0);
 }
