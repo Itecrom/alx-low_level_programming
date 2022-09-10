1fa64b288784c8e2fa824854f0ab27413523cb2a
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-10 07:58:34 +0200

    Causes un ending iteration

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Results in an unending iteration
+ * Return: Zero
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Incoming-unending iteration :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("nUending iteration broked \\o/\n");
+
+	return (0);
+}
