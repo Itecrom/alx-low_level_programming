4dd08280e25dde1714ce0c34c347317fc5adeba7
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-10 07:47:16 +0200

    a testing function for -/+ int)

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function outputs (positive or negative)
+ * Return: Zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
