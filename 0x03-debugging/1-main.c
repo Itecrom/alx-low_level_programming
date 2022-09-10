8eaa1293416aeaada247e96b79b16ed84534b515
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-10 08:04:06 +0200

    Causes un ending iteration

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Results in an unending iteration
- * Return: Zero
+ * main - causes an infinite loop
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
 
-	printf("Incoming-unending iteration :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	printf("nUending iteration broked \\o/\n");
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
