bc1e00c0a9faef4da7cc0efa5c0319a4f8c8696c
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-12 00:58:04 +0200

    More Pointers, Arrays and Strings by Leonard JJ Mhone-ALX-SE

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat -  function that concatenates two strings.
+ * @src: the source
+ * @dest: the destination
+ * @n: number.
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = strlen(dest);
+	int a = 0;
+
+	while (a < n && *src)
+	{
+		dest[index + a] = *src;
+		src++;
+		a++;
+	}
+	dest[index + a] = '\0';
+	return (dest);
+}
