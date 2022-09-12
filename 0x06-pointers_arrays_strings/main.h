593c467be22b91d926ca6dd90772e8cd62eeef1a
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-12 03:39:58 +0200

    Print Numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef _main_h_
-#define _main_h_
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,5 +8,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-
-#endif
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
