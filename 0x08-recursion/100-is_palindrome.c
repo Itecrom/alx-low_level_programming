9c244e3439f4fe28b447571b1f7c9552e0772aed
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-09-28 15:05:39 +0200

    Palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 
 int find_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
-int is_palindrome(char *s);
+int is_palindrom(char *s);
 
 /**
  * find_strlen - Returns the length of a string.
