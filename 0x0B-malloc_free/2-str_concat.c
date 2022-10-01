f2df4a7ca4cd1f612ff66f08e00e30f980d4aa23
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-10-01 18:35:44 +0200

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
+ * str_concat - Concatenates 2 strings.
  * @s1: The string to be concatenated upon.
  * @s2: The string to be concatenated to s1.
  *
