b4eea9f347edc5f8e635fa53cc5c8c59f7ce254a
Leonard JJ Mhone <leonardjjmhone@gmail.com>
2022-10-10 16:39:01 +0200

    The Macros are back

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,7 +2,7 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /*
- * File: 3-function_like_macro.h
+ * File: 3-function_like_macro
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */
